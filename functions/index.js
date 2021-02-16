ed4493589d2ed36fa20ca7bf3cc8bacdefb78089
EmiYoda <emilianodelvalle@icloud.com>
2021-02-16 11:50:49 -0600

    fixed some typos

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,17 @@
 const functions = require("firebase-functions");
-const express = require('express');
-const { default: firebase } = require("firebase");
-const admin = require('firebase-admin')
+const express = require("express");
+const admin = require("firebase-admin");
 
 const app = express();
 admin.initializeApp({
-    credentials: admin.credential.cert('./permissions.json'),
-    databaseURL: 'https://photodb-api.firebaseio.com'
-})
-
-app.get('/hello-world', (req, res) => {
-    return res.status(200).json({message: 'Hello world'})
+  credentials: admin.credential.cert("./permissions.json"),
+  databaseURL: "https://photodb-api.firebaseio.com",
 });
 
-app.use(require('./routes/products.routes'))
+app.get("/hello-world", (req, res) => {
+  return res.status(200).json({message: "Hello world"});
+});
+
+app.use(require("./routes/products.routes"));
 
 exports.app = functions.https.onRequest(app);
--- functions/routes/products.routes.js
+++ functions/routes/products.routes.js
@@ -1,15 +1,15 @@
-const { Router } = require('express')
-const router = Router()
+const { Router } = require("express");
+const router = Router();
 
-const { default: firebase } = require("firebase");
-const admin = require('firebase-admin')
+
+const admin = require("firebase-admin");
 
 const db = admin.firestore();
 
-router.post('/api/products', async (req, res) => {
+router.post("/api/products", async (req, res) => {
     try {
-        await db.collection('products')
-        .doc('/' + req.body.id + '/')
+        await db.collection("products")
+        .doc('/' + req.body.id + "/")
         .create({name: req.body.name});
     return res.status(204).json();
     } catch (error) {
@@ -18,50 +18,50 @@ router.post('/api/products', async (req, res) => {
     }
 });
 
-router.get('/api/products/:product_id', async (req, res) => {
+router.get("/api/products/:product_id", async (req, res) => {
     try {
-        const doc = db.collection('products').doc(req.params.product_id);
-        const item = await doc.get()
-        const response = item.data()
-        return res.status(200).json(response)
+        const doc = db.collection("products").doc(req.params.product_id);
+        const item = await doc.get();
+        const response = item.data();
+        return res.status(200).json(response);
     } catch (error) {
         console.log(error);
         return res.status(500).send(error);
     }
-})
+});
 
-router.get('/api/products', async (req, res) => {
+router.get("/api/products", async (req, res) => {
     try {
-        const query = db.collection('products');
+        const query = db.collection("products");
         const querySnapshot = await query.get();
         const docs = querySnapshot.docs;
     
         const response = docs.map(doc => ({
             id: doc.id,
             name: doc.data().name,
-        }))
+        }));
     
-        return res.status(200).json(response)
+        return res.status(200).json(response);
     } catch (error) {
         console.log(error);
-        return res.status(500).send(error)
+        return res.status(500).send(error);
     }
-})
+});
 
-router.delete('/api/products/:product_id', async (req, res) => {
+router.delete("/api/products/:product_id", async (req, res) => {
     try {
-        const document = db.collection('products').doc(req.params.product_id)
-        await document.delete()
-        return res.status(200).json()
+        const document = db.collection("products").doc(req.params.product_id);
+        await document.delete();
+        return res.status(200).json();
     } catch (error) {
         console.log(error);
         return res.status(500).json(error);
     }
-})
+});
 
-router.put('/api/products/:product_id', async (req, res) => {
+router.put("/api/products/:product_id", async (req, res) => {
     try {
-        const document = db.collection('products').doc(req.params.product_id)
+        const document = db.collection("products").doc(req.params.product_id);
         await document.update({
             name: req.body.name,
         });
@@ -70,6 +70,6 @@ router.put('/api/products/:product_id', async (req, res) => {
     } catch (error) {
         return res.status(500).json();
     }
-})
+});
 
-module.exports = router
+module.exports = router;
